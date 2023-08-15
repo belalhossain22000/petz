24667eb51061eed5038b2f3f6441595e8aa92dff
SUMON <belalhossain22000@gmail.comgit config --global user.name>
2023-08-16 00:52:48 +0600

    added responsive for all device

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ const navItems = document.querySelector('.nav-items');
 hamburger.addEventListener('click', () => {
   navItems.classList.toggle('active');
 });
+
+// can do slider
+const slider = document.querySelector('.slider');
+const slides = document.querySelectorAll('.slide');
+let currentIndex = 0;
+
+function updateSlider() {
+  slider.style.transform = `translateX(-${currentIndex * 100}%)`;
+}
+
+function prevSlide() {
+  currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+  updateSlider();
+}
+
+function nextSlide() {
+  currentIndex = (currentIndex + 1) % slides.length;
+  updateSlider();
+}
+
