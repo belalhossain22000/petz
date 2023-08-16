50b5115bd1ad0f0c47a1e3b2730a0076b5b3507c
SUMON <belalhossain22000@gmail.comgit config --global user.name>
2023-08-16 21:32:43 +0600

    added slider category

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
-const hamburger = document.querySelector('.hamburger');
-const navItems = document.querySelector('.nav-items');
+const hamburger = document.querySelector(".hamburger");
+const navItems = document.querySelector(".nav-items");
 
-hamburger.addEventListener('click', () => {
-  navItems.classList.toggle('active');
+hamburger.addEventListener("click", () => {
+  navItems.classList.toggle("active");
 });
 
 // -------------------can do slider--------------------------
+const sliderContainer = document.querySelector(".slider-container");
+const prevButton = document.querySelector(".slider-prev");
+const nextButton = document.querySelector(".slider-next");
+const sliderItems = document.querySelectorAll(".slider-item");
+let currentIndex = 0;
+
+function updateSliderPosition() {
+  const itemWidth = sliderItems[0].offsetWidth;
+  const newPosition = -currentIndex * itemWidth;
+  sliderContainer.style.transform = `translateX(${newPosition}px)`;
+}
+
+prevButton.addEventListener("click", () => {
+  currentIndex = Math.max(currentIndex - 1, 0);
+  updateSliderPosition();
+});
+
+nextButton.addEventListener("click", () => {
+  currentIndex = Math.min(currentIndex + 1, sliderItems.length - 3);
+  updateSliderPosition();
+});
